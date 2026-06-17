1beaa136b71e863107ba1f5d0bd107a69bf33edd
Jhencee <jhenceeborjal16@gmail.com>
2026-06-17 17:30:12 +0800

    Configure mail (Gmail SMTP / Resend transport) and test sending

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\Transport\ResendTransport;
+use Resend;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Mail::extend('resend', function () {
+            return new ResendTransport(
+                Resend::client(config('services.resend.key'))
+            );
+        });
     }
 }
